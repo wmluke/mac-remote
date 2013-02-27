4668ce9809749ebb1bd4bc248b95f00bf6a71c08
Luke Bunselmeyer <wmlukeb@gmail.com>
2013-02-27 01:45:21 -0800

    Added new arq command line functions to pause and resume backup.  Added some bootstrap ui love.

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express'),
     http = require('http'),
     path = require('path'),
     exec = require('child_process').exec,
-    Q = require('q');
+    _ = require('underscore');
+
+_.str = require('underscore.string');
 
 var app = express();
 
 app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT || 9000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
@@ -23,6 +21,7 @@ app.configure(function () {
     app.use(app.router);
     app.use(require('less-middleware')({ src: __dirname + '/public' }));
     app.use(express.static(path.join(__dirname, 'public')));
+    app.locals.pretty = true;
 });
 
 app.configure('development', function () {
@@ -41,21 +40,11 @@ app.get('/backup/pause', function (req, res) {
             console.info(stdout);
         }
     });
-    exec('killall arqcommitter', function (error, stdout, stderr) {
-        if (error) {
-            console.info(stderr);
-        } else {
-            console.info(stdout);
-        }
-    });
-    exec('killall Arq', function (error, stdout, stderr) {
-        if (error) {
-            console.info(stderr);
-        } else {
-            console.info(stdout);
-        }
-    });
-    exec('killall "Arq Agent"', function (error, stdout, stderr) {
+    /**
+     * Arq.app/Contents/MacOS/Arq
+     * 'backupnow', 'pause' and 'resume' command-line functions
+     */
+    exec('/Applications/Arq.app/Contents/MacOS/Arq pause 120', function (error, stdout, stderr) {
         if (error) {
             console.info(stderr);
         } else {
@@ -73,16 +62,35 @@ app.get('/backup/start', function (req, res) {
             console.info(stdout);
         }
     });
-    exec('open /Applications/Arq.app', function (error, stdout, stderr) {
+    exec('/Applications/Arq.app/Contents/MacOS/Arq resume', function (error, stdout, stderr) {
         if (error) {
             console.info(stderr);
         } else {
             console.info(stdout);
         }
     });
-
     res.redirect('/');
 });
+
+/**
+ * Probably vulnerable to inject attacks.  Enable at your own risk.
+ */
+//app.get('/say/:something', function (req, res) {
+//    var something = req.params.something || '';
+//    if (_.str.isBlank(req.params.something)) {
+//        res.send(400);
+//        return;
+//    }
+//    var sanitized = _.str.humanize(something.replace(/[^\d\w\s]/gi, ''));
+//    exec('say "' + something + '"', function (error, stdout, stderr) {
+//        if (error) {
+//            console.info(stderr);
+//        } else {
+//            console.info(stdout);
+//        }
+//    });
+//    res.send(200);
+//});
 
 
 http.createServer(app).listen(app.get('port'), function () {
